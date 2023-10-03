76139b862ebd39e2a7e09ce41df89c852650d189
beatrizgomess <beatrizgomesxx@gmail.com>
2023-10-02 21:55:39 -0300

    Prática 02 - Parte 01 - Criando novas telas

--- app/src/main/java/com/pdm/weatherapp/FavoritePage.kt
+++ app/src/main/java/com/pdm/weatherapp/FavoritePage.kt
@@ -1,42 +1,37 @@
 package com.pdm.weatherapp
 
-import android.app.Activity
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview(showBackground = true)
+
 @Composable
-fun HomePage(modifier: Modifier = Modifier){
-    val activity = LocalContext.current as? Activity
+fun FavoritePage(name: String, modifier: Modifier = Modifier) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(colorResource(id = R.color.teal_700)).wrapContentSize(Alignment.Center)
+            .background(colorResource(id = R.color.black))
+            .wrapContentSize(Alignment.Center)
     ) {
-        Text(text = "Home",
+        Text(
+            text = "Favoritas",
             fontWeight = FontWeight.Bold,
             color = Color.White,
             modifier = Modifier.align(Alignment.CenterHorizontally),
             textAlign = TextAlign.Center,
-            fontSize = 20.sp)
-        Spacer(modifier = Modifier.size(24.dp))
+            fontSize = 20.sp
+        )
     }
-
-
 }
+
